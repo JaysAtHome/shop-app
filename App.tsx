08e52ae02092f58726c3c8e353d87c58f3f84117
JayM <jay_marlo_marmeto@dlsl.edu.ph>
2025-03-07 12:57:21 +0800

    commit #2

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { CartProvider } from './context/CartContext';
 import HomeScreen from './screens/homeScreen';
 import CartScreen from './screens/cartScreen';
 import CheckoutScreen from './screens/checkoutScreen';
@@ -9,12 +10,14 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Shop Now' }} />
-        <Stack.Screen name="Cart" component={CartScreen} options={{ title: 'Your Cart' }} />
-        <Stack.Screen name="Checkout" component={CheckoutScreen} options={{ title: 'Checkout' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Cart" component={CartScreen} />
+          <Stack.Screen name="Checkout" component={CheckoutScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
 }
--- context/CartContext.js
+++ context/CartContext.js
@@ -1,60 +1,37 @@
-import React, { createContext, useReducer, useContext } from 'react';
+import React, { createContext, useState } from 'react';
 
-const CartContext = createContext();
-
-const initialState = {
-  cart: [],
-};
-
-const cartReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TO_CART':
-      const existingItem = state.cart.find(item => item.id === action.payload.id);
-      if (existingItem) {
-        return {
-          ...state,
-          cart: state.cart.map(item =>
-            item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
-          ),
-        };
-      } else {
-        return {
-          ...state,
-          cart: [...state.cart, { ...action.payload, quantity: 1 }],
-        };
-      }
-    case 'REMOVE_FROM_CART':
-      return {
-        ...state,
-        cart: state.cart.filter(item => item.id !== action.payload.id),
-      };
-    case 'INCREMENT_QUANTITY':
-      return {
-        ...state,
-        cart: state.cart.map(item =>
-          item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
-        ),
-      };
-    case 'DECREMENT_QUANTITY':
-      return {
-        ...state,
-        cart: state.cart.map(item =>
-          item.id === action.payload.id ? { ...item, quantity: item.quantity - 1 } : item
-        ).filter(item => item.quantity > 0),
-      };
-    default:
-      return state;
-  }
-};
+export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(cartReducer, initialState);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      }
+      return [...prevCart, { ...product, quantity: 1 }];
+    });
+  };
+
+  const updateQuantity = (id, action) => {
+    setCart((prevCart) =>
+      prevCart
+        .map((item) =>
+          item.id === id
+            ? { ...item, quantity: action === 'increase' ? item.quantity + 1 : item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
 
   return (
-    <CartContext.Provider value={{ state, dispatch }}>
+    <CartContext.Provider value={{ cart, addToCart, updateQuantity, setCart }}>
       {children}
     </CartContext.Provider>
   );
 };
-
-export const useCart = () => useContext(CartContext);
